946cf7f2499e702110ddf58406429647e9a8f61a
John Solo <johnsolojoseph@gmail.com>
2019-04-07 15:32:43 -0700

    Created mouseover and mouseout events

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 M.AutoInit();
+
+function changeToGif(img, fileName) {
+  img.src = 'images/' + fileName + '.gif';
+}
+
+function changeToPNG(img, fileName) {
+  img.src = 'images/' + fileName + '.png';
+}
