b09b31749f003fce1d1867bdaa153aba38da1676
unknown <johnsolojoseph@gmail.com>
2020-09-05 16:43:16 -0700

    Styled and organized home page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import '../styles/header.scss';
-import ProfilePic from './ProfilePic';
 import avatarUrl from '../assets/profile.jpg';
+
+import ProfilePic from './ProfilePic';
+import RoundedBtn from './RoundedBtn';
+
+
 
 function Header({name, title}){
     return(
@@ -10,6 +14,10 @@ function Header({name, title}){
             <ProfilePic avatarUrl={avatarUrl} alt="Self portrait of John in Last Bookstore" />
             <h1 className="name">{name}</h1>
             <h1 className="title">{title}</h1>
+            <div className="button-list">
+                <RoundedBtn text="my portfolio" />
+                <RoundedBtn text="about me" />
+            </div>
         </div>
     
     );
