57e8c8e2f4b98eb05fa301c467cecefe7496d9d9
unknown <johnsolojoseph@gmail.com>
2020-09-06 00:46:21 -0700

    Portfolio modals

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import '../styles/portfolio.scss';
 
 import socialMapsImg from '../assets/socialmaps.png';
+import imageBoxImg from '../assets/imagebox.png';
+import hiliteImg from '../assets/hi-lite.png';
+import csclubImg from '../assets/csclub.png';
+import ytImg from '../assets/youtube-socket.png';
+import pictoImg from '../assets/picto-find.png';
 
 
 import NavBar from './NavBar';
@@ -20,31 +25,45 @@ function Portfolio(){
                 <div className="column">
                     <Project
                     imageUrl={socialMapsImg}
+                    title="Social CrowdMapping"
+                    description="
+                    Created a community-driven map with location-dependent data using Google Maps API and Firebase. 
+                    Optimized update and delete operations by implementing a HashMap to store map marker objects"
                     />
                 </div>
                 <div className="column">
                     <Project
-                    imageUrl={socialMapsImg}
+                    imageUrl={csclubImg}
+                    title="Computer Science Department Club Web App"
+                    description="Integrated an event calendar system with Google Calendar API to increase social interaction among club members and highlight upcoming events"
                     />
                 </div>
                 <div className="column">
                     <Project
-                    imageUrl={socialMapsImg}
+                    imageUrl={imageBoxImg}
+                    title="Browser-Based Photo Editor"
+                    description="Developed a ReactJS web-based developer tool that allows end-users to edit pictures with CSS"
                     />
                 </div>
                 <div className="column">
                     <Project
-                    imageUrl={socialMapsImg}
+                    imageUrl={hiliteImg}
+                    title="Hi-Lite"
+                    description="Built an application that generates a quick summary of posted blogs and news articles using the MEAN stack"
                     />
                 </div>
                 <div className="column">
                     <Project
-                    imageUrl={socialMapsImg}
+                    imageUrl={ytImg}
+                    title="Synchronized YouTube Videos"
+                    description="Employed a real-time engine with Socket.io, which allowed users to sync embedded YouTube videos together at the same time point and load the same video across multiple clients"
                     />
                 </div>
                 <div className="column">
                     <Project
-                    imageUrl={socialMapsImg}
+                    imageUrl={pictoImg}
+                    title="Google Image Clone"
+                    description="Created a Google Images clone with ReactJS and utilized modern CSS Flexbox"
                     />
                 </div>
                 
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import '../styles/project.scss';
 
+import Modal from './Modal';
+
 function Project({title, description, imageUrl, alt}){
+    const[showModal, setShowModal] = useState(false);
+
+    function toggle(){
+      setShowModal(!showModal);
+      
+    }
+
     return(
         <div className="project">
-            <div className="project-img">
+            <div 
+            className="project-img" 
+            role="button" 
+            onClick={toggle} 
+            onKeyDown={toggle} 
+            tabIndex={0}>
                 <img src={imageUrl} alt={alt}/>
             </div>
+
+            <Modal 
+            showModal={showModal} 
+            toggle={toggle}
+            title={title}
+            description={description}
+            imageUrl={imageUrl}
+            />
           
         </div>
     );
