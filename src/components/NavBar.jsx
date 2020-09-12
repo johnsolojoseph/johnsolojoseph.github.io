a06ad9595b61889353ede0d828ef85f49b40f786
unknown <johnsolojoseph@gmail.com>
2020-09-12 15:36:33 -0700

    sidenav with close button

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import '../styles/navbar.scss';
 
-import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 
 function NavBar(){
     const [navWidth, setNavWidth] = useState(0);
@@ -17,32 +17,36 @@ function NavBar(){
             <nav className="desktop=nav">
                 <ul className="nav-desktop-ul">
                     <li>
-                        <Link to="/" >Home</Link>
+                        <NavLink to="/" >Home</NavLink>
                     </li>
                     <li>
-                        <Link to="/portfolio">Portfolio</Link>
+                        <NavLink to="/portfolio">Portfolio</NavLink>
                     </li>
                     <li>
-                        <Link to="/about">About</Link>
+                        <NavLink to="/about">About</NavLink>
                     </li>
                 </ul>
                 
             </nav>
 
-            <i className="fas fa-bars" onClick={() => setNavWidth(100)} onKeyDown={() => handleKeyDown(100)} role="button" tabIndex="0"/>
+            <i className="fas fa-bars burger" onClick={() => setNavWidth(100)} onKeyDown={() => handleKeyDown(100)} role="button" tabIndex="0"/>
 
             <div className="side-nav" style={{width:`${ navWidth }%`}}>
-                <ul className="nav-mobile-ul">
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/portfolio">Portfolio</Link>
-                    </li>
-                    <li>
-                        <Link to="/about">About</Link>
-                    </li>
-                </ul>
+                <div className="mobile-container ">
+                    <i className="fas fa-times" onClick={() => setNavWidth(0)} onKeyDown={() => handleKeyDown(0)} role="button" tabIndex="-1"/>
+                    <ul className="nav-mobile-ul">
+                        <li>
+                            <NavLink to="/">Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/portfolio">Portfolio</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/about">About</NavLink>
+                        </li>
+                    </ul>
+                </div>
+                
             </div>
         </div>
 
