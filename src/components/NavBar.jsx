4310d610dab17577e24db59f7493691dca8cce2b
unknown <johnsolojoseph@gmail.com>
2020-09-12 14:53:28 -0700

    creating sidenav component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/navbar.scss';
 
 import {Link} from 'react-router-dom';
 
 function NavBar(){
+    const [navWidth, setNavWidth] = useState(0);
+
+    function handleKeyDown(width) {
+        setNavWidth(width)
+    }
+
+    
+
     return(
         <div className="navbar">
-            <nav>
+            <nav className="desktop=nav">
                 <ul className="nav-desktop-ul">
+                    <li>
+                        <Link to="/" >Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/portfolio">Portfolio</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                </ul>
+                
+            </nav>
+
+            <i className="fas fa-bars" onClick={() => setNavWidth(100)} onKeyDown={() => handleKeyDown(100)} role="button" tabIndex="0"/>
+
+            <div className="side-nav" style={{width:`${ navWidth }%`}}>
+                <ul className="nav-mobile-ul">
                     <li>
                         <Link to="/">Home</Link>
                     </li>
@@ -18,9 +43,10 @@ function NavBar(){
                         <Link to="/about">About</Link>
                     </li>
                 </ul>
-                <i className="fas fa-bars" />
-            </nav>
+            </div>
         </div>
+
+        
     );
 }
 
