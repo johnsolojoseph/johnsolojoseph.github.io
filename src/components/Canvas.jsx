1ec861ac8d7564df7aeec5649c61d9fb4aed2875
unknown <johnsolojoseph@gmail.com>
2020-09-12 18:31:38 -0700

    particle js for landing page

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -5,49 +5,58 @@ import Particles from 'react-particles-js';
 function Canvas(){
     return(
         <Particles
-        params={{
-          "particles": {
-              "number": {
-                  "value": 60,
-                  "density": {
-                      "enable": true,
-                      "value_area": 1500
-                  }
-              },
-              "line_linked": {
-                  "enable": true,
-                  "opacity": 0.02
-              },
-              "move": {
-                  "direction": "right",
-                  "speed": 0.05
-              },
-              "size": {
-                  "value": 1
-              },
-              "opacity": {
-                  "anim": {
-                      "enable": true,
-                      "speed": 1,
-                      "opacity_min": 0.05
-                  }
-              }
-          },
-          "interactivity": {
-              "events": {
-                  "onclick": {
-                      "enable": true,
-                      "mode": "push"
-                  }
-              },
-              "modes": {
-                  "push": {
-                      "particles_nb": 1
-                  }
-              }
-          },
-          "retina_detect": true
-        }} />
+            className="canvas"
+            params={{
+                "particles": {
+                    "number": {
+                        "value": 160,
+                        "density": {
+                            "enable": false
+                        }
+                    },
+                    "size": {
+                        "value": 3,
+                        "random": true,
+                        "anim": {
+                            "speed": 4,
+                            "size_min": 0.3
+                        }
+                    },
+                    "line_linked": {
+                        "enable": false
+                    },
+                    "move": {
+                        "random": true,
+                        "speed": 1,
+                        "direction": "top",
+                        "out_mode": "out"
+                    }
+                },
+                "interactivity": {
+                    "events": {
+                        "onhover": {
+                            "enable": true,
+                            "mode": "bubble"
+                        },
+                        "onclick": {
+                            "enable": true,
+                            "mode": "repulse"
+                        }
+                    },
+                    "modes": {
+                        "bubble": {
+                            "distance": 250,
+                            "duration": 2,
+                            "size": 0,
+                            "opacity": 0
+                        },
+                        "repulse": {
+                            "distance": 400,
+                            "duration": 4
+                        }
+                    }
+                }
+            }} />
     );
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,8 @@ import '../styles/home.scss';
 
 import Header from './Header';
 
+import Canvas from './Canvas';
+
 function Home(){
     return(
         <div className="home">
@@ -10,6 +12,8 @@ function Home(){
             name="John Solo"
             title="Software Engineer"
             />
+
+            <Canvas />
         </div>
     );
 }
